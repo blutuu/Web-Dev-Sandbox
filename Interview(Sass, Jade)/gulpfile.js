974d2c31bc708294fe28e78c8fbcf8fc0b50a180
Marlin <blutuu1@gmail.com>
2018-08-05 17:26:36 -0400

    More project initializing.

---
+++ Interview(Sass, Jade)/gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	jade = require('gulp-jade'),
+	jshint = require('gulp-jshint'),
+	plumber = require('gulp-plumber'),
+	notify = require('gulp-notify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	webserver = require('gulp-webserver');
+
+// Manage JS
+gulp.task('js', function() {
+	return gulp.src('builds/js/**/*.js')
+
+		.pipe(plumber({ errorHandler: function(err) {
+
+            notify.onError({
+                title: "Gulp error in " + err.plugin,
+                message:  err.toString()
+            })(err);
+
+        }}))
+
+		.pipe(jshint('./.jshintrc'))
+	    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Manage Sass
+gulp.task('sass', function() {
+	return gulp.src('process/sass/style.scss')
+
+		.pipe(plumber({ errorHandler: function(err) {
+
+            notify.onError({
+                title: "Gulp error in " + err.plugin,
+                message:  err.toString()
+            })(err);
+
+        }}))
+
+		.pipe(sourcemaps.init())
+		.pipe(sass({outputStyle: 'Expanded'}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('builds/css'));
+});
+
+// Watching files for changes
+gulp.task('watch', function() {
+	gulp.watch('builds/js/**/*', ['js']);
+	gulp.watch(['process/sass/**/*.scss'], ['sass']);
+});
+
+// Starting the live server
+gulp.task('webserver', function() {
+    gulp.src('builds/')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
+});
+
+gulp.task('default', ['sass', 'watch', 'webserver']);
